b9654c12a6539b5f16a367e28019a9249e42fe00
earmitag <emma.armitage@gov.bc.ca>
2026-03-12 12:08:19 -0700

    params for coastal cutthroat trout

---
+++ model/01_access/sql/model_access_cct.sql
@@ -0,0 +1,197 @@
+with barriers as
+(
+  select
+    barriers_gradient_id as barrier_id,
+    barrier_type,
+    barrier_name,
+    linear_feature_id,
+    blue_line_key,
+    downstream_route_measure,
+    wscode_ltree,
+    localcode_ltree,
+    watershed_group_code,
+    geom
+  from bcfishpass.barriers_gradient
+  where watershed_group_code = :'wsg'
+  -- and barrier_type in ('GRADIENT_20', 'GRADIENT_25', 'GRADIENT_30')
+  and barrier_type in ('GRADIENT_30')
+  union all
+  select
+      barriers_falls_id as barrier_id,
+      barrier_type,
+      barrier_name,
+      linear_feature_id,
+      blue_line_key,
+      downstream_route_measure,
+      wscode_ltree,
+      localcode_ltree,
+      watershed_group_code,
+      geom
+  from bcfishpass.barriers_falls
+  where watershed_group_code = :'wsg'
+  union all
+  select
+      barriers_subsurfaceflow_id as barrier_id,
+      barrier_type,
+      barrier_name,
+      linear_feature_id,
+      blue_line_key,
+      downstream_route_measure,
+      wscode_ltree,
+      localcode_ltree,
+      watershed_group_code,
+      geom
+  from bcfishpass.barriers_subsurfaceflow
+  where watershed_group_code = :'wsg'
+),
+
+obs_upstr as
+(
+  select
+    b.barrier_id,
+    b.barrier_type,
+    b.blue_line_key,
+    b.downstream_route_measure,
+    b.watershed_group_code,
+    o.species_code as spp,
+    o.observation_key as obs,
+    o.observation_date as obs_dt
+  from barriers b
+  inner join bcfishpass.observations o
+  on fwa_upstream(
+        b.blue_line_key,
+        b.downstream_route_measure,
+        b.wscode_ltree,
+        b.localcode_ltree,
+        o.blue_line_key,
+        o.downstream_route_measure,
+        o.wscode,
+        o.localcode,
+        false,
+        20   -- a large tolerance to discard observations at more or less the same location as the barrier (within 20m)
+      )
+  -- do not bother counting observations upstream of barriers that have been noted as barriers in the user control table
+  left outer join bcfishpass.user_barriers_definite_control bc
+  on b.blue_line_key = bc.blue_line_key and abs(b.downstream_route_measure - bc.downstream_route_measure) < 1
+  -- where o.species_code in ('CH','CM','CO','PK','SK','ST')
+  where o.species_code in ('ACT', 'CCT', 'CT')
+  and bc.barrier_ind is null
+),
+
+obs_upstr_n as
+(
+  select
+    o.barrier_id,
+    count(o.obs) as n_obs
+  from obs_upstr o
+  -- where o.spp in ('CH','CM','CO','PK','SK','ST')
+  where o.spp in ('ACT', 'CCT', 'CT')
+  and o.obs_dt > date('1990-01-01')         -- only observations since 1990
+  group by o.barrier_id
+),
+
+-- exclude barriers belown known spawning/rearing habitat
+habitat as (
+  select
+    h.blue_line_key,
+    h.upstream_route_measure,
+    s.wscode_ltree,
+    s.localcode_ltree,
+    h.watershed_group_code,
+    h.species_code
+  from bcfishpass.user_habitat_classification h
+  inner join whse_basemapping.fwa_stream_networks_sp s
+  ON s.blue_line_key = h.blue_line_key
+  and round(h.upstream_route_measure::numeric) >= round(s.downstream_route_measure::numeric)
+  and round(h.upstream_route_measure::numeric) <= round(s.upstream_route_measure::numeric)
+  where h.habitat_ind is true
+  -- and h.species_code in ('CH','CM','CO','PK','SK','ST')
+  and h.species_code in ('ACT', 'CCT', 'CT')
+  and s.watershed_group_code = :'wsg'
+),
+
+hab_upstr as
+(
+  select
+    b.barrier_id,
+    array_agg(species_code) as species_codes
+  from barriers b
+  inner join habitat h
+  on fwa_upstream(
+        b.blue_line_key,
+        b.downstream_route_measure,
+        b.wscode_ltree,
+        b.localcode_ltree,
+        h.blue_line_key,
+        h.upstream_route_measure,
+        h.wscode_ltree,
+        h.localcode_ltree,
+        false,
+        200  -- a v large tolerance to discard habitat that ends at more or less the same location as the barrier
+      )
+  group by b.barrier_id
+),
+
+barriers_filtered as (
+  select
+    b.barrier_id as barrier_load_id,
+    barrier_type,
+    barrier_name,
+    linear_feature_id,
+    blue_line_key,
+    downstream_route_measure,
+    wscode_ltree,
+    localcode_ltree,
+    watershed_group_code,
+    geom
+  from barriers b
+  left outer join obs_upstr_n as o on b.barrier_id = o.barrier_id
+  left outer join hab_upstr h on b.barrier_id = h.barrier_id
+  where watershed_group_code = any(
+      array(
+        select watershed_group_code
+        from bcfishpass.wsg_species_presence
+        -- where st is true - replace for where cct is true??
+        where st is true
+      )
+  )
+  -- do not include gradient / falls / subsurface barriers with > 5 observations upstream
+  -- but always include user added barriers
+  and
+        (
+          (o.n_obs is null or o.n_obs < 5) and
+          h.species_codes is null
+  )
+)
+
+insert into bcfishpass.barriers_cct
+(
+    barriers_cct_id,
+    barrier_type,
+    barrier_name,
+    linear_feature_id,
+    blue_line_key,
+    downstream_route_measure,
+    wscode_ltree,
+    localcode_ltree,
+    watershed_group_code,
+    geom
+)
+select * from barriers_filtered
+union all
+select
+    barriers_user_definite_id as barrier_load_id,
+    barrier_type,
+    barrier_name,
+    linear_feature_id,
+    blue_line_key,
+    downstream_route_measure,
+    wscode_ltree,
+    localcode_ltree,
+    watershed_group_code,
+    geom
+from bcfishpass.barriers_user_definite
+where watershed_group_code = :'wsg'
+-- where watershed_group_code in
+-- ('ALBN','ATLL','ATNA','BABL','BABR','BARR','BELA','BRKS','BULK','CAMB','CHIR','CHWK','CLAY','COMX','COWN','DRIR','FRAN','FRCN','GOLD','GRAI','HARR','HOLB','HOMA','INKR','ISKR','JERV','KAKC','KEEC','KHTZ','KINR','KISP','KITL','KITR','KLAR','KLIN','KLUM','KNIG','KSHR','KTSU','KUMR','KUSR','LBIR','LDEN','LFRA','LILL','LISR','LKEL','LNAR','LNIC','LRDO','LSKE','LSTR','MBNK','MESC','MORI','MORR','MSKE','MSTR','NAHR','NAKR','NASC','NASR','NBNK','NECL','NECR','NEVI','NIEL','NIMP','OWIK','PARK','PORI','SALM','SANJ','SETN','SEYM','SHER','SIML','SKGT','SPAT','SQAM','STIR','STUL','TAHR','TAHS','TAKL','TASR','TATR','TAYR','TOBA','TSAY','TSIT','TUTR','TUYR','UBIR','UDEN','UEUT','UNAR','UNRS','UNUR','USKE','UTRE','VICT','WORC','ZYMO')
+on conflict do nothing;
