8628264170d05aee1dade9af8dbb767e21cda35b
earmitag <emma.armitage@gov.bc.ca>
2026-03-27 16:28:26 -0700

    only include observations newer that 1990

--- model/01_access/sql/update_fiss_habitat.sql
+++ model/01_access/sql/update_fiss_habitat.sql
@@ -1,7 +1,7 @@
 with fiss_source_wscode as (
     select distinct v.wscode from bcfishpass.streams_vw v
     join bcfishpass.observations o on o.linear_feature_id = v.linear_feature_id
-    where v.access_st = 1 and o.species_code = 'CT' and o.source ILIKE '%FISS%'
+    where v.access_st = 1 and o.species_code = 'CT' and o.source ILIKE '%FISS%' and o.obs_dt > date('1990-01-01')
     and not exists (
         select 1
         from bcfishpass.streams_upstr_observations ou
